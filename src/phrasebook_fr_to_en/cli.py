e2dfb9642f0fb6860dd401c2b11bf6a40088ac4b
Tony Aldon <tony@tonyaldon.com>
2026-02-04 10:46:14 +0100

    release version 0.3.0

--- src/phrasebook_fr_to_en/cli.py
+++ src/phrasebook_fr_to_en/cli.py
@@ -11,7 +11,7 @@ from watchdog.events import FileSystemEvent, FileSystemEventHandler
 from watchdog.observers import Observer
 
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 if TYPE_CHECKING:
     from pydantic import BaseModel
